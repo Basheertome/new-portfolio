1136b32248b2121ac6b07120fe67969d02509e53
Basheer Tome <basheer@google.com>
2016-09-06 00:37:36 -0400

    Deleted homepage and about page. Moved everything to student fork with new header

--- index.js
+++ index.js
@@ -1,49 +1,8 @@
 $(document).ready(function(){
 
-	if (window.location.hash.split('#')[1] == 'projects') {
-		window.location.replace("#");  
-		if (typeof window.history.replaceState == 'function') {
-		  history.replaceState({}, '', window.location.href.slice(0, -1));
-		}
-		$(window).scrollTop($('.intro').height()-100);
-	}
-
-    $('.arrow').click(function() {
-    	$('body, html').animate({scrollTop: $('.intro').height()-100});
-    });
-
-    $('audio')[0].addEventListener('playing', function() {
-    	$('.mute').fadeIn();
-    });
-    $('.mute').click(function() {
-    	var audiostate = $('audio')[0].muted;
-    	if (audiostate == true) {
-    		$('audio')[0].muted = false;
-    		for (var i=0; i<20; i++) {
-    			$('.mute').animate({'background-position-x': -912+i*48 + 'px'}, 0).delay(30);
-    		}
-    	} else {
-    		$('audio')[0].muted = true;
-    		for (var i=0; i<20; i++) {
-    			$('.mute').animate({'background-position-x': i*-48 + 'px'}, 0).delay(30);
-    		}
-    	}
-    });
-
-	$('.intro').animate({'opacity': 1}, 1000);
-
 	if ('ontouchstart' in document.documentElement) {
 		$('html').addClass('touch');
 	}
-
-	$(window).scroll(function(event) { 
-		percent = 1 - (($(window).scrollTop() / $('.intro').height()*2));
-		if (percent >= 1) {
-			$('.intro').css({'opacity': 1});
-		} else if (percent >= 0) {
-			$('.intro').css({'opacity': percent});
-		} else { $('.intro').css({'opacity': 0}); }
-	});
 
 	hoverx = new Array($('.thumbnails').find('a').length);
 	for (var i=0; i<hoverx.length; i++) {
