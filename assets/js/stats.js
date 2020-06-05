4d1b3cfef1a4366cfcf064523ba5fc1a0a156e21
Basheer Tome <basheer@basheertome.com>
2020-06-05 16:20:41 -0700

    Switched analytics to Fathom

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,1 +1,11 @@
-$.getScript('https://stats.basheer.co/?js');
+(function(f, a, t, h, o, m){
+	a[h]=a[h]||function(){
+		(a[h].q=a[h].q||[]).push(arguments)
+	};
+	o=f.createElement('script'),
+	m=f.getElementsByTagName('script')[0];
+	o.async=1; o.src=t; o.id='fathom-script';
+	m.parentNode.insertBefore(o,m)
+})(document, window, '//stats.basheer.co/tracker.js', 'fathom');
+fathom('set', 'siteId', 'OTTTC');
+fathom('trackPageview');
